865d5eaf7d4ccabba7cc1b52ce3db118e714c7b6
Caroline de Paula Oliveira <caroline.oliveira@selbetti.local>
2026-05-21 13:43:48 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('olá mundo');
+let numeroSecreto = 29;
+let chute = prompt('escolha um numero entre 1 e 30');
+
+if (numeroSecreto == chute){
+    console.log('Isso ai vc descobriu o numero secreto(29)')
+}
