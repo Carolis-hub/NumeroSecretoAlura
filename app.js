07e10d1d896f5f2960ff9dcc4967756c0d6b1fea
Caroline de Paula Oliveira <caroline.oliveira@selbetti.local>
2026-05-24 10:41:59 -0300

    Commit final

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-alert('olá mundo');
-let numeroSecreto = 29;
-let chute = prompt('escolha um numero entre 1 e 30');
+alert('boas vindas ao jogo do numero secreto!!!');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute){
-    console.log('Isso ai vc descobriu o numero secreto(29)')
+while(chute != numeroSecreto){
+    chute = prompt(`escolha um numero entre 1 e ${numeroMaximo}`);
+    if (numeroSecreto == chute){
+        //alert(`Isso ai vc descobriu o numero secreto ${numeroSecreto}`);
+        break;
+    } else{
+        if (numeroSecreto > chute){
+            alert(`NS é maior q o chute de ${chute}`);  
+        } else{
+            alert(`NS é menor q o chute de ${chute}`);     
+        }
+        tentativas++;
+    }
+
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : tentativa;
+    alert(`Isso ai vc descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 }
